7c09cdcc461a57d95dfbebcd0baa38c43bce726b
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-20 21:57:20 -0400

    part 5 done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,23 +11,12 @@ import { MessageService } from '../message.service';
 
 export class HeroesComponent implements OnInit {
   
-  
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
-  }
-    
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-
-  
-
 
   constructor(private heroService: HeroService, private messageService: MessageService) {  }
 
